91ddc16a69152d7aee69d7843430513690704bc6
Tolga BAYRAK <bayraktolga28@gmail.com>
2026-01-12 21:48:37 +0300

    se

--- server/app/repositories/auth_repository.py
+++ server/app/repositories/auth_repository.py
@@ -14,11 +14,9 @@ class AuthRepository(BaseRepository[User]):
         return self.db.query(User).filter(User.email == email).first()
 
     def verify_user_credentials(self, email: str, password: str) -> Optional[User]:
-        """Verify user credentials and return user if valid"""
         user = self.get_by_email(email)
         return user
 
     def create_user(self, email: str, hashed_password: str) -> User:
-        """Create a new user with hashed password"""
         user = User(email=email, password=hashed_password)
         return self.create(user)
